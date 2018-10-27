34f460fbd6717f170ff0cda42363f6d3653b2acb
Benjamin Wu <benjaminwu124@gmail.com>
2018-10-26 23:46:50 -0400

    Make history view collapsible

--- src/components/HistoryView.jsx
+++ src/components/HistoryView.jsx
@@ -3,16 +3,32 @@ import React from 'react';
 import '../styles/history.scss';
 
 class HistoryView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapsed: false
+    };
+
+    this.onHeaderClicked = this.onHeaderClicked.bind(this);
+  }
+
+  onHeaderClicked() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+
   render() {
-    const icon = this.props.collapsed ? '▴' : '▾';
+    const icon = this.state.collapsed ? '▴' : '▾';
 
     return (
       <div className="history-view">
-        <div className="header">
+        <div className="header" onClick={this.onHeaderClicked}>
           <div className="title">History</div>
           <div className="icon">{icon}</div>
         </div>
-        <div className="line-container">
+        <div className={`line-container ${this.state.collapsed ? 'collapsed' : ''}`}>
           {this.props.lines.map((line, i) =>
             <div className="history-line" key={i}>
               <span className="caret">>> </span>{line}
